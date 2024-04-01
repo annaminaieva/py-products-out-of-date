d9df5bb36bfb023cc578bd66ea4358829a3d5ffa
Anna Minaieva <annaminaieva213@gmail.com>
2024-04-01 12:30:39 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,85 @@
-# write your code here
+import datetime
+import pytest
+from typing import Callable
+from unittest import mock
+
+from app.main import outdated_products
+
+
+@mock.patch("app.main.datetime.date")
+@pytest.mark.parametrize(
+    "products, today_return, expected_result",
+    [
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 2, 10),
+                    "price": 600
+                },
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 5),
+                    "price": 120
+                },
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160
+                }
+            ],
+            datetime.date(2022, 2, 5),
+            ["duck"]
+        ),
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 2, 10),
+                    "price": 600
+                },
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 5),
+                    "price": 120
+                },
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160
+                }
+            ],
+            datetime.date(2022, 2, 11),
+            ["salmon", "chicken", "duck"]
+        ),
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 2, 10),
+                    "price": 600
+                },
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 5),
+                    "price": 120
+                },
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160
+                }
+            ],
+            datetime.date(2022, 1, 30),
+            []
+        )
+    ]
+)
+def test_outdated_products(
+        mocked_datetime_date: Callable,
+        products: list,
+        today_return: datetime,
+        expected_result: list
+) -> None:
+    mocked_datetime_date.today = mock.Mock(return_value=today_return)
+    assert outdated_products(products) == expected_result
